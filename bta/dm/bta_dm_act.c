3be41f7f65ae1f7f34de70d7be348c7849439f40
Yan Laijun <yan.laijun@gmail.com>
2016-01-04 17:26:07 +0800

    Check if role switch should be avoided before finding a link
    Also removed unecessary compile time flag for it.
    
    Change-Id: I4e67468c348194216e533ffb02a0375046322534
    Signed-off-by: yanlaijun <yan.laijun@gmail.com>

--- bta/dm/bta_dm_act.c
+++ bta/dm/bta_dm_act.c
@@ -3203,45 +3203,36 @@ static void bta_dm_rs_cback (tBTM_ROLE_SWITCH_CMPL *p1)
 *******************************************************************************/
 static BOOLEAN bta_dm_check_av(UINT16 event)
 {
-    BOOLEAN avoid_roleswitch = FALSE;
     BOOLEAN switching = FALSE;
-    UINT8 i;
-    tBTA_DM_PEER_DEVICE *p_dev;
 
-#if defined(BTA_DM_AVOID_A2DP_ROLESWITCH_ON_INQUIRY) && (BTA_DM_AVOID_A2DP_ROLESWITCH_ON_INQUIRY == TRUE)
-
-    /* avoid role switch upon inquiry if a2dp is actively streaming as it
-       introduces an audioglitch due to FW scheduling delays (unavoidable) */
+    /* Avoid role switch upon inquiry if A2DP is actively streaming, as it
+       introduces an audio glitch due to FW scheduling delays (unavoidable). */
     if (event == BTA_DM_API_SEARCH_EVT)
-    {
-        avoid_roleswitch = TRUE;
-    }
-#endif
+        return FALSE;
 
-    APPL_TRACE_WARNING("bta_dm_check_av:%d", bta_dm_cb.cur_av_count);
-    if(bta_dm_cb.cur_av_count)
+    APPL_TRACE_WARNING("%s cur_av_count: %d", __func__, bta_dm_cb.cur_av_count);
+    if (bta_dm_cb.cur_av_count == 0)
+        return FALSE;
+
+    for(int i = 0; i < bta_dm_cb.device_list.count; i++)
     {
-        for(i=0; i<bta_dm_cb.device_list.count; i++)
+        const tBTA_DM_PEER_DEVICE *p_dev = &bta_dm_cb.device_list.peer_device[i];
+        if ((p_dev->conn_state == BTA_DM_CONNECTED) && (p_dev->info & BTA_DM_DI_AV_ACTIVE))
         {
-            p_dev = &bta_dm_cb.device_list.peer_device[i];
-            APPL_TRACE_WARNING("[%d]: state:%d, info:x%x, avoid_rs %d",
-                                i, p_dev->conn_state, p_dev->info, avoid_roleswitch);
-            if((p_dev->conn_state == BTA_DM_CONNECTED) && (p_dev->info & BTA_DM_DI_AV_ACTIVE) &&
-               (avoid_roleswitch == FALSE))
+            /* make master and take away the role switch policy */
+            int rc = BTM_SwitchRole(p_dev->peer_bdaddr, HCI_ROLE_MASTER, (tBTM_CMPL_CB *)bta_dm_rs_cback);
+            if (rc == BTM_CMD_STARTED)
             {
-                /* make master and take away the role switch policy */
-                if(BTM_CMD_STARTED == BTM_SwitchRole (p_dev->peer_bdaddr, HCI_ROLE_MASTER, (tBTM_CMPL_CB *)bta_dm_rs_cback))
-                {
-                    /* the role switch command is actually sent */
-                    bta_dm_cb.rs_event = event;
-                    switching = TRUE;
-                }
-                /* else either already master or can not switch for some reasons */
-                bta_dm_policy_cback(BTA_SYS_PLCY_CLR, 0, HCI_ENABLE_MASTER_SLAVE_SWITCH, p_dev->peer_bdaddr);
-                break;
+                /* the role switch command is actually sent */
+                bta_dm_cb.rs_event = event;
+                switching = TRUE;
             }
+            /* else either already master or can not switch for some reasons */
+            bta_dm_policy_cback(BTA_SYS_PLCY_CLR, 0, HCI_ENABLE_MASTER_SLAVE_SWITCH, p_dev->peer_bdaddr);
+            break;
         }
     }
+
     return switching;
 }
 
